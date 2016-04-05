f64eb5a8feed2bd86ef75ed6d8ee1dcfab8cc920
Srdjan Mladjenovic <mladjenovic.srdjan@live.com>
2016-04-05 22:20:41 +0200

    stavka otkupnog lista

---
+++ Magacinsko poslovanje/Modeli/StavkaOtkupnogLista.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Modeli
+{
+    public class StavkaOtkupnogLista
+    {
+        [Required(ErrorMessage = "Redni broj stavke otkupnog lista mora biti unet")]
+        [Key]
+        [Column(Order = 2)]
+        public int RedniBrojStavke { get; set; }
+        public double JedCena { get; set; }
+        public double Kolicina { get; set; }
+        public double UkupnaCena { get; set; }
+        public string JedMere { get; set; }
+        [Required]
+        public int KlasaID { get; set; }
+        public virtual Klasa Klasa { get; set; }
+        [Required]
+        public int VrstaID { get; set; }
+        public virtual Vrsta Vrsta { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        [ForeignKey("OtkupniList")]
+        public long DokumentID { get; set; }
+        public virtual OtkupniList OtkupniList { get; set; }
+
+        public StavkaOtkupnogLista()
+        { }
+    }
+}
